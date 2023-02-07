f9da28b3454bfd69537291b5d36290c981b2c9e6
bententhhousand <bententhhousand@gmail.com>
2023-02-06 19:56:26 -0500

    Project Initialization
    Implementation of Vue.js, integration of material design bootstrap, setup of basic navbar, setup of links, set up of routing framework

---
+++ B2RFrontend/src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import IndexView from '../views/IndexView.vue'
+import ProgressView from '../views/ProgressView.vue'
+import AdminConsoleView from '../views/AdminConsoleView.vue'
+import ContactsView from '../views/ContactsView.vue'
+import OrgchartView from '../views/OrgchartView.vue'
+import ProfileView from '../views/ProfileView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: IndexView
+    },
+    {
+      path: '/progress',
+      name: 'progress',
+      component: ProgressView
+    },
+    {
+      path: '/adminconsole',
+      name: 'adminconsole',
+      component: AdminConsoleView
+    },
+    {
+      path: '/contacts',
+      name: 'contacts',
+      component: ContactsView
+    },
+    {
+      path: '/orgchart',
+      name: 'orgchart',
+      component: OrgchartView
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView
+    }
+  ]
+})
+
+export default router
---
+++ B2RFrontend/src/views/ProgressView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    Progress View
+  </main>
+</template>
