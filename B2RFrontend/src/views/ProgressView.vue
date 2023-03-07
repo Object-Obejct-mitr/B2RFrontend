505de3de402d7551b59d35a67dfc6a9f02340c35
James Liu <liuxjames88@gmail.com>
2023-03-07 17:58:57 -0500

    drag and drop implemented now

--- B2RFrontend/src/views/ProgressView.vue
+++ B2RFrontend/src/views/ProgressView.vue
@@ -1,28 +1,42 @@
 
 <template>
     <main>   
-        <div :key="drag" class="d-flex flex-column" >
+        <div class="d-flex flex-column" >
             
-            <div v-for="(taskContent, name) in tasks" :key="taskContent" class="h-100 d-flex align-items-center mt-5 flex-fill">
+            <div v-for="(name) in tasks" :key="name" class="h-100 d-flex align-items-center mt-5 flex-fill">
                 <div class="d-flex justify-content-center flex-column flex-fill">
                     <h1 class="text-center">{{ name }}</h1>
                     <div class="d-flex flex-row flex-fill justify-content-around">  
                         <div class="d-flex flex-column flex-fill ms-5 me-auto w-50">
                             <h2 class="text-center">To-Do</h2>
                             <draggable 
-                                v-model="tasks[name]['To-Do']"
+                                v-model="todo[name]"
                                 :group="name"
+                                item-key="id"  
+                                @start="drag = true"
+                                @end="drag = false"
                                 @change="onChange" >
-                                <Task v-for="taskName in tasks[name]['To-Do']" :key="taskName" :component="taskName"/>
+                                <template #item="{ element }">
+                                    <div>
+                                        <Task :component="element.name" />
+                                    </div>
+                                </template>
                             </draggable>
                         </div>
                         <div class="d-flex flex-column flex-fill ms-5 me-auto w-50">
                             <h2 class="text-center">Done</h2>
                             <draggable 
-                                v-model="tasks[name]['Done']"
+                                v-model="done[name]"
                                 :group="name"
+                                item-key="id"  
+                                @start="drag = true"
+                                @end="drag = false"
                                 @change="onChange" >
-                                <Task v-for="taskName in tasks[name]['Done']" :key="taskName" :component="taskName"/>
+                                <template #item="{ element }">
+                                    <div>
+                                        <Task :component="element.name" />
+                                    </div>
+                                </template>
                             </draggable>
                         </div>
                     </div>
@@ -35,61 +49,58 @@
 
 <script lang="js">
 import Task from "@/components/ProgressView/Task.vue";
-import { VueDraggableNext } from "vue-draggable-next";
-import { defineComponent } from "vue";
+import { toRaw } from "vue";
+import draggable from 'vuedraggable'
 
 
-export default defineComponent({
+export default {
     name: "ProgressView",
     components: {
         Task,
-        draggable: VueDraggableNext
+        draggable
     },
     data() {
         return {
+            test: 0,
             drag: false,
-            tasks: {
-                Drivetrain: {
-                    "To-Do": [
-                        "Component 0",
-                        "Component 1",
-                        "Component 2",
-                        "Component 3",
-                        "Component 4"
-                    ],
-                    "Done": [
-                        "Component 5",
-                        "Component 6",
-                        "Component 7"
-                    ]
-                },
-                Electronics: {
-                    "To-Do": [
-                        "Component 0",
-                        "Component 1",
-                        "Component 2",
-                        "Component 3",
-                        "Component 4"
-                    ],
-                    "Done": [
-                        "Component 5",
-                        "Component 6",
-                        "Component 7"
-                    ]
-                }
-            }
+            todo: {
+                Drivetrain: [
+                    {name: "Component 0", id: 0},
+                    {name:"Component 1", id:1},
+                    {name:"Component 2", id:2},
+                    {name:"Component 3", id:3},
+                    {name:"Component 4",id: 4}
+                ]
+            },
+            done: {
+                Drivetrain: [
+                    {name:"Component 5", id:5},
+                    {name:"Component 6", id:6},
+                    {name:"Component 7", id:7}
+                ]
+            },
+            tasks: [
+                "Drivetrain",
+                "Electronics"
+            ]   
         }
 
+    },
+    mounted() {
+        console.log("yeah")
     },
     methods: {
         onChange(event) {
             console.log(event)
-            console.log(this.tasks)
+            console.log(toRaw(this.todo))
+            console.log(toRaw(this.done))
             // this.drag = !this.drag
+            // this.$forceUpdate();
+            console.log(this.test++);
         }
     }
 
-})
+}
 
 
 
