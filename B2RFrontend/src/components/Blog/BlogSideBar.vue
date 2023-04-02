575a9dbfad32c6a58a1dddb8abb0e75062ab9987
BriLo0916 <briannalop916@gmail.com>
2023-04-02 17:17:50 -0400

    created modal

--- B2RFrontend/src/components/Blog/BlogSideBar.vue
+++ B2RFrontend/src/components/Blog/BlogSideBar.vue
@@ -2,13 +2,11 @@
     <div>
         
         <div v-if="view == 'list'" class = "sideBar">
-            <button class="buttonSpacing btn btn-secondary" @click="createModifyPost()">New Post</button>
             <h2>Filter by Date</h2>
             <hr />
             <h2>Filter by Tags</h2>
         </div>
         <div v-else class = "sideBar" >
-            <button class="buttonSpacing btn btn-secondary" @click="createModifyPost()">Edit Post</button>
             <h2>Recent Posts</h2>
         </div>
         <!-- <button @click="debug">
@@ -31,36 +29,10 @@ export default {
             default: "list",
         },
     },
-    emits: ["createModifyPost"],
     methods: {
         debug() {
             console.log(this.view);
         },
-        createModifyPost() {
-            if (this.view == 'list') {
-                //create a post
-                this.$emit("createModifyPost", "create");
-            } else {
-                //edit a post
-                this.$emit("createModifyPost", "modify");
-            }
-        }
     },
 };
 </script>
-
-<style>
-.sideBar {
-    display: flex;
-    flex-direction: column;
-}
-
-.buttonSpacing {
-    margin: 0 auto;
-    margin-top: 10%; 
-    margin-bottom: 10%; 
-    padding-bottom: 15%;
-    width: fit-content;
-}
-
-</style>
--- B2RFrontend/src/views/IndexView.vue
+++ B2RFrontend/src/views/IndexView.vue
@@ -9,13 +9,39 @@
             :posts="blogData"
             @show-post="showPost"
         />
-        <BlogSideBar
-            id="blogSideBar"
-            :view="view"
-            :posts="blogData"
-            :tags="tags"
-            @create-modify-post="createModifyPost"
-        />
+        <div class="sideBar">
+    
+            <button v-if="view == 'list'" class="buttonSpacing btn btn-secondary" @click="createModifyPost('create')" data-mdb-toggle='modal' data-mdb-target='#createModal'>New Post</button>
+            <button v-else class="buttonSpacing btn btn-secondary" @click="createModifyPost('modify')" data-mdb-toggle='modal' data-mdb-target='#createModal'>Edit Post</button>
+    
+            <BlogSideBar
+                id="blogSideBar"
+                :view="view"
+                :posts="blogData"
+                :tags="tags"
+                
+            />
+        </div>
+        <!-- create/modify modal -->
+        <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 v-if="view == 'list'" class="modal-title" id="exampleModalLabel1">Create Post</h5>
+                        <h5 v-if="view == 'post'" class="modal-title" id="exampleModalLabel2">Modify Post</h5>
+                        <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <label>Title: </label>
+                        <!-- <input v-model="postData.title"> -->
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
     <button @click="debug()">switch views</button>
 </template>
@@ -36,7 +62,8 @@ export default {
             view: "list",
             blogData: BlogData,
             tags: Tags,
-            postIndex: null,
+            postIndex: 0,
+            postData: null,
         };
     },
     mounted() {},
@@ -53,11 +80,15 @@ export default {
         createModifyPost(postType) {
             if (postType == "create") {
                 //create a post
-                console.log("creating a post")
+                console.log("creating a post");
+                
+                
             } else if (postType == "modify") {
                 //edit a post
-                console.log("emitting a post")
-
+                console.log("editing a post")
+                this.postData = this.blogData[this.postIndex];
+                
+                
             }
         }
     },
@@ -90,5 +121,27 @@ export default {
     width: 20vw;
     flex-grow: 1;
     height: fit-content;
+    
+}
+
+.buttonSpacing {
+    margin: 0 auto;
+    margin-top: 10%; 
+    margin-bottom: 10%; 
+    padding-bottom: 15%;
+    width: fit-content;
+}
+
+.sideBar {
+    display: flex;
+    flex-direction: column;
+}
+
+.buttonSpacing {
+    margin: 0 auto;
+    margin-top: 10%; 
+    margin-bottom: 10%; 
+    padding-bottom: 15%;
+    width: fit-content;
 }
 </style>
