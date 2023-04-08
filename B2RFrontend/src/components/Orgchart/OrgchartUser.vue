45b8853a5a1361464a7a8a4607b8d0e0066ebb22
Jonathan Kloepfer <kloepj@rpi.edu>
2023-04-08 03:14:25 -0400

    Work On hooking up Org Chart

--- B2RFrontend/src/components/Orgchart/OrgchartUser.vue
+++ B2RFrontend/src/components/Orgchart/OrgchartUser.vue
@@ -3,17 +3,27 @@
         <div class="card-body">
             <div class="d-flex flex-column align-items-center">
                 <img
-                    src="https://mdbootstrap.com/img/new/avatars/8.jpg"
+                    :src="imgsrc"
                     alt=""
                     style="width: 150px; height: 150px"
                     class="rounded-circle"
                 />
                 <div class="text-center">
-                    <p class="fw-bold mb-1">John Doe</p>
-                    <p class="text-muted mb-0">john.doe@gmail.com</p>
-                    <p class="text-muted mb-0">(555)-555-5555</p>
+                    <p class="fw-bold mb-1">{{name}}</p>
+                    <p class="text-muted mb-0">{{email}}</p>
+                    <p class="text-muted mb-0" v-if="nubmer!=''" >{{number}}</p>
                 </div>
             </div>
         </div>
     </div>
 </template>
+<script>
+ export default{
+    setup(props){
+      const imgsrc=props.user.photoURL;
+      const name=props.user.displayName;
+      const email=props.user.email;
+      const number=props.user.phoneNumber;
+     }
+}
+     </script>
--- B2RFrontend/src/views/OrgchartView.vue
+++ B2RFrontend/src/views/OrgchartView.vue
@@ -1,113 +1,154 @@
-<script setup>
+<!-- <script> -->
+<!-- </script> -->
+<template>
+    <div class="d-flex flex-row">
+        <Sidebar />
+    </div>
+</template>
+<!-- <template>
+     <main>
+     <div class="d-flex flex-row">
+     <Sidebar />
+     <div class="d-flex flex-column flex-fill">
+     <div class="d-flex flex-column mt-5">
+     <h1 class="text-center">Team Mentors</h1>
+     <div class="row mt-3">
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     </div>
+     <div class="row mt-5">
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     </div>
+     </div>
+     <div class="d-flex flex-column mt-5">
+     <h1 class="text-center">Team Captains</h1>
+     <div class="row mt-3">
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     </div>
+     </div>
+     <div class="d-flex flex-column mt-5">
+     <h1 class="text-center">Component Heads</h1>
+     <div class="row mt-3">
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     </div>
+     </div>
+     <div class="d-flex flex-column mt-5">
+     <h1 class="text-center">Manufacturers</h1>
+     <div class="row mt-3">
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     </div>
+     <div class="row mt-5">
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     </div>
+     <div class="row mt-5">
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     </div>
+     <div class="row mt-5">
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     <div class="col">
+     <OrgchartUser />
+     </div>
+     </div>
+     </div>
+     </div>
+     </div>
+     </main>
+     </template> -->
+<script>
 import OrgchartUser from "../components/Orgchart/OrgchartUser.vue";
 import Sidebar from "../components/Navbar/Sidebar.vue";
+ import { db } from "../firebase";
+import { collection, doc, getDocs,setDoc, query, where } from "firebase/firestore";
+import { ref, computed, watchEffect } from 'vue';
+ export default{
+     setup(){
+         const catigories=ref({});
+         const fetchUsers = async () => {
+         const catigoryList= collection(db,"OrgChart");
+         const querySnapshot = await getDocs(catigoryList);
+        catigories.value={};
+        querySnapshot.forEach(doc => {
+        catigories.value[doc.data().Position]=[];
+            // for(let user in doc.data().Contact){
+            // console.log(user.data());
+            // console.log("HI");
+            // }
+
+            var s=doc.data()
+            console.log(s.Contact);
+            const companyDocumentSnapShot2 = db.doc(s.Contact[0].path).get().await();
+            // if(s.Contact[0]){s.Contact[0].get().then(res=>{
+            // console.log(res.data())
+            // })}
+            // if(!users.value.includes(user)){
+            //     console.log(users.value);
+            //     users.value.push(user);
+            // }
+        });
+      };
+        fetchUsers();
+         // watchEffect(() => {
+         // fetchUsers();
+         // });
+
+
+     }
+ }
 </script>
-
-<template>
-    <main>
-        <div class="d-flex flex-row">
-            <Sidebar />
-            <div class="d-flex flex-column flex-fill">
-                <div class="d-flex flex-column mt-5">
-                    <h1 class="text-center">Team Mentors</h1>
-                    <div class="row mt-3">
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                    </div>
-                    <div class="row mt-5">
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                    </div>
-                </div>
-                <div class="d-flex flex-column mt-5">
-                    <h1 class="text-center">Team Captains</h1>
-                    <div class="row mt-3">
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                    </div>
-                </div>
-                <div class="d-flex flex-column mt-5">
-                    <h1 class="text-center">Component Heads</h1>
-                    <div class="row mt-3">
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                    </div>
-                </div>
-                <div class="d-flex flex-column mt-5">
-                    <h1 class="text-center">Manufacturers</h1>
-                    <div class="row mt-3">
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                    </div>
-                    <div class="row mt-5">
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                    </div>
-                    <div class="row mt-5">
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                    </div>
-                    <div class="row mt-5">
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                        <div class="col">
-                            <OrgchartUser />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
-</template>
-
-<style scoped></style>
