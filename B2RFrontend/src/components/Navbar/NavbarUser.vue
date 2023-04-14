5efa79eec48c9a7569c38f1d982e1b7b11879ec8
bententhhousand <bententhhousand@gmail.com>
2023-04-14 01:24:20 -0400

    Rewrote View Profile + Signin

--- B2RFrontend/src/components/Navbar/NavbarUser.vue
+++ B2RFrontend/src/components/Navbar/NavbarUser.vue
@@ -1,206 +1,266 @@
-<script setup>
+<script>
 import { onMounted } from "vue";
+
+import { auth, db, storage } from "../../firebase";
+import { ref } from "vue";
+import { uploadBytesResumable, ref as firebaseRef, getStorage, getDownloadURL, uploadBytes, deleteObject } from 'firebase/storage'
+import { collection, getDocs, query, where, addDoc, doc, updateDoc } from "firebase/firestore"
+import InputText from "../Misc/InputText.vue";
+import InputPhoneNumber from "../Misc/InputPhoneNumber.vue";
 import router from "../../../src/router";
 import { signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, setPersistence, browserSessionPersistence } from "firebase/auth";
-import { auth } from "../../firebase";
-
-onMounted(async () => {
-    //If a user exists in local storage, pull from there
-    // console.log(JSON.stringify(localStorage.getItem("user")))
-    if (localStorage.getItem("user") != undefined) {
-        //console.log(JSON.parse(localStorage.getItem("user")));
-        //populateUsername(JSON.parse(localStorage.getItem("user")));
-        user.value = JSON.parse(localStorage.getItem("user"));
-        signedIn.value = true;
-        //updateUsername(JSON.parse(localStorage.getItem("user")));
-
-    } else {
-        //Else, get the fresh user
-        onAuthStateChanged(auth, (user) => {
-            updateUsername(user);
-        });
-        // let newUser = await getCurrentUser();
-        // updateUsername(newUser);
-    }
-
-});
-
-</script>
-
-<script>
-import { auth, db } from "../../firebase";
-import { ref } from "vue";
-import {uploadBytes, ref as storageRef, getStorage,getDownloadURL} from 'firebase/storage'
-import { collection, getDocs, query, where, addDoc,doc, setDoc } from "firebase/firestore"
-const signedIn = ref(false); 
-const user = ref(undefined);
-const imageData=ref(null);
-const picture= ref(null);
-const uploadValue= ref(0);
-const usersCollection = collection(db, "users");
-const file=ref(null);
-const get2= (event)=>{
-     file.value=event.target.files[0];
- }
-const profileEditor= ()=> {
-     var pronouns=document.getElementById("Pronouns").value.trim();
-     const storage = getStorage();
-     const storageR = storageRef(storage,"/userpictures/"+user.value.uid);
-     if(pronouns!=""){
-         user.value.pronouns=pronouns;
-     }
-// 'file' comes from the Blob or File API
-uploadBytes(storageR,file.value).then((snapshot) => {
-  console.log('Uploaded a blob or file!');
-});
-getDownloadURL(storageRef(storage,"/userpictures/"+user.value.uid)).then((url)=>{
-     user.value.photoURL=url;
-     const docRef = doc(db, "users", user.value.uid);
-     console.log(user.value.uid);
-setDoc(docRef, user.value)
-.then(docRef => {
-    console.log("Entire Document has been updated successfully");
-})
-.catch(error => {
-    console.log(error);
-})
-
-
- })
-}
-
-async function checkExistingUser(user) {
-
-    
-    // Default user constructor 
-    const userData = {
-        displayName: user.providerData[0].displayName,
-        permissions: [],
-        photoURL: user.providerData[0].photoURL,
-        pronouns: "No Pronouns Set",
-        role: "Student",
-        uid: user.providerData[0].uid,
-        email: user.providerData[0].email
-    }
-    console.log(userData)
-    const q = query(usersCollection, where("uid", "==", userData.uid));
-    const querySnapshot = await getDocs(q);
-    if (querySnapshot.empty) {
-        console.log("User does not exist, creating...")
-        await addDoc(collection(db, "users"), {
-            displayName: userData.displayName, 
-            permissions: userData.permissions,
-            photoURL: userData.photoURL,
-            pronouns: userData.pronouns,
-            role: userData.role,
-            uid: userData.uid,
-            email: userData.email
-        });
-        return userData;
-    } else {
-        console.log("User exists")
-        console.log(querySnapshot.docs[0].data())
-        return querySnapshot.docs[0].data()
-    }
-
-}
-
-async function updateUsername(newUser) {
-    if (newUser != undefined) {
-        user.value = await checkExistingUser(newUser)
-        signedIn.value = true;
-        localStorage.setItem("user", JSON.stringify(user.value))
-    } else {
-        user.value = undefined;
-        signedIn.value = false;
-        localStorage.removeItem("user")
-    }
-};
+import { useToast, POSITION } from "vue-toastification";
 
 const provider = new GoogleAuthProvider();
-const signIn = () => {
-    setPersistence(auth, browserSessionPersistence)
-    signInWithPopup(auth, provider)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            updateUsername(result.user);
-        })
-        .catch((error) => {
-            console.log(error);
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            const email = error.customData.email;
-            const credential = GoogleAuthProvider.credentialFromError(error);
-        });
-};
+export default {
+    components: {
+        InputText,
+        InputPhoneNumber
+    },
+    data() {
+        return {
+            userValue: undefined,
+            signedInNew: false,
+            profileImageFile: undefined,
+            profileImageNewLink: undefined
+        }
+    },
+    setup() {
+        const toast = useToast();
+        return { toast }
+    },
+    mounted() {
+        console.log("Fetching User");
+        const data = localStorage.getItem('user');
+        console.log(data)
+        if (data != undefined) {
+            console.log("Found user in local storage");
+            this.userValue = JSON.parse(data);
+            this.signedInNew = true;
+        }
 
-const signOutDriver = () => {
-    signOut(auth).then(
-        function () {
-            console.log("Signed Out");
-            signedIn.value = false;
-            user.value = undefined;
-            router.go(0);
-            localStorage.removeItem("user")
+    },
+    methods: {
+        async saveChanges() {
+            const userColl = collection(db, "users");
+            const q = query(collection(db, "users"), where("email", "==", this.userValue.email));
+            const querySnapshot = await getDocs(q);
+            const docID = querySnapshot.docs[0].id;
+            if (this.userValue.phoneNumber == undefined){
+                this.userValue.phoneNumber = "";
+            }
+            if (this.userValue.pronouns == undefined){
+                this.userValue.pronouns = "";
+            }
+
+
+            await updateDoc(doc(userColl, docID), {
+                    displayName: this.userValue.displayName,
+                    pronouns: this.userValue.pronouns,
+                    phoneNumber: this.userValue.phoneNumber,
+                    photoURL: this.userValue.photoURL
+                }).then(() => {
+                    this.toast.success("Updated Profile Successfully", {
+                        timeout: 2000,
+                        position: POSITION.BOTTOM_RIGHT
+                    })
+                    localStorage.setItem("user", JSON.stringify(this.userValue))
+                    $("#navBarModal").modal('hide');
+                }).catch(error => {
+                    console.log("Error, " + error)
+                    this.toast.error("An Error Occurred", {
+                        timeout: 10000,
+                        position: POSITION.BOTTOM_RIGHT
+                    });
+                });
 
         },
-        function (error) {
-            console.error("Sign Out Error", error);
-            localStorage.removeItem("user")
-        }
-    );
-};
+        undoChanges() {
+            if(this.profileImageFile!=undefined){
+                let fileName = firebaseRef(storage, this.profileImageNewLink).name;
+                let fileRef = firebaseRef(storage, 'profilePhotos/'+fileName);
+                deleteObject(fileRef).then(()=>{
+                    console.log("Delete Success");
+                });
+            }
+            this.userValue = JSON.parse(localStorage.getItem('user'));
+            this.profileImageFile = undefined;
+            this.profileImageNewLink = undefined;
+            $("#navBarModal").modal('hide');
+        }, 
+        async changeProfileImage() {
+            // Upload new photo to Firestore
+            const storageRef = firebaseRef(storage, 'profilePhotos/'+ this.userValue.uid + this.profileImageFile.name); //
+            // 'file' comes from the Blob or File API
+
+            const uploadTask = uploadBytesResumable(storageRef, this.profileImageFile);
+            uploadTask.on('state_changed', (snapshot) => {
+                // Handle state changes if needed
+            }, (error) => {
+                this.toast.error("Error Uploading Files", {
+                    timeout: 10000,
+                    position: POSITION.BOTTOM_RIGHT
+                });
+            }, async () => {
+                let newURL = await getDownloadURL(uploadTask.snapshot.ref);
+                this.userValue.photoURL = newURL;
+                this.profileImageNewLink = newURL;
+            });
+        },
+        async onProfilePhotoSelected(event) {
+            this.profileImageFile = event.target.files[0];
+            await this.changeProfileImage();
+        },
+        async checkExistingUser(user) {
+            // Check to see if a user exists in our local database.
+
+            const userData = {
+                displayName: user.providerData[0].displayName,
+                permissions: [],
+                photoURL: user.providerData[0].photoURL,
+                pronouns: "",
+                role: "Student",
+                uid: user.providerData[0].uid,
+                email: user.providerData[0].email,
+                phoneNumber: ""
+            }
+            console.log(userData)
+            const q = query(collection(db, "users"), where("email", "==", userData.email));
+            const querySnapshot = await getDocs(q);
+            if (querySnapshot.empty) {
+                // User doesn't exist
+                console.log("User does not exist, creating...")
+                await addDoc(collection(db, "users"), {
+                    displayName: userData.displayName,
+                    permissions: userData.permissions,
+                    photoURL: userData.photoURL,
+                    pronouns: userData.pronouns,
+                    role: userData.role,
+                    uid: userData.uid,
+                    email: userData.email,
+                    phoneNumber: userData.phoneNumber
+                });
+                return userData;
+            } else {
+                // User does exist
+                console.log("User exists")
+                console.log(querySnapshot.docs[0].data())
+                return querySnapshot.docs[0].data()
+            }
+
+        },
+        async handleSignIn(newUser) {
+            if (newUser != undefined) {
+                // If the user isn't undefined (error check)
+                this.userValue = await this.checkExistingUser(newUser)
+                this.signedInNew = true;
+                localStorage.setItem("user", JSON.stringify(this.userValue))
+            } else {
+                user.value = undefined;
+                signedIn.value = false;
+                localStorage.removeItem("user")
+            }
+        },
+        signOutDriver() {
+            signOut(auth).then(() => {
+                console.log("Signed Out");
+                this.signedInNew = false;
+                this.userValue = undefined;
+                router.go(0);
+                localStorage.removeItem("user")
+            }).catch((error) => {
+                console.log("Signout Error, ");
+                console.log(error)
+            });
+        },
+        async signInDriver() {
+            setPersistence(auth, browserSessionPersistence);
+            await signInWithPopup(auth, provider)
+                .then((result) => {
+                    // This gives you a Google Access Token. You can use it to access the Google API.
+                    const credential = GoogleAuthProvider.credentialFromResult(result);
+                    const token = credential.accessToken;
+                    this.handleSignIn(result.user);
+                })
+                .catch((error) => {
+                    console.log(error);
+                    // Handle Errors here.
+                });
+        },
+        handlePFPClick() {
+            this.$refs.fileInput.click();
+        },
+
+    }
+
+}
 
 </script>
 
 <template>
-    <div v-if="!signedIn" class="d-flex align-items-center">
-        <a class="btn text-white me-2 btn-success" @click="signIn" href="#" role="button">
+    <div v-if="!this.signedInNew" class="d-flex align-items-center">
+        <a class="btn text-white me-2 btn-success" @click="signInDriver" href="#" role="button">
             <i class="fab fa-google me-2"></i>
             Sign In With School Email
         </a>
 
     </div>
-    <div v-if="signedIn" class="d-flex align-items-center">
+    <div v-if="this.signedInNew" class="d-flex align-items-center">
         <div class="d-flex flex-column align-items-center">
-            <h4>Welcome, {{ user.displayName }}</h4>
+            <h4>Welcome, {{ this.userValue.displayName }}</h4>
             <div class="d-flex flex-row">
-                <button class="btn btn-primary px-3" data-mdb-toggle="modal" data-mdb-target="#exampleModal">
-                        View Profile
-                    </button>
+                <button class="btn btn-primary px-3" data-mdb-toggle="modal" data-mdb-target="#navBarModal">
+                    View Profile
+                </button>
                 <button class="btn btn-link px-3" @click="signOutDriver">
                     Sign Out
                 </button>
             </div>
         </div>
-        <img :src="user.photoURL" referrerpolicy="no-referrer" class="rounded-circle px-3" height="80" />
-    </div>
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Update Profile</h5>
-        <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
 
-<div class="form-outline">
-    <label class="form-label" for="customFile">Please Upload New Profile Picture</label>
-    <input type="file" class="form-control" id="customFile" ref="ProfilePic" @change="get2" accept="image/*"/>
-</div>
-<div class="form-outline">
-  <input id="Pronouns" type="text" class="form-control form-control-lg" />
-  <label class="form-label" for="Pronouns" >Enter new pronouns</label>
-</div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" @click="profileEditor">Save changes</button>
-      </div>
+        <img :src="this.userValue.photoURL" referrerpolicy="no-referrer" class="rounded-circle px-3" height="80" />
+        <div class="modal fade" id="navBarModal" tabindex="-1" aria-labelledby="navBarModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="navBarModalLabel">Update Profile</h5>
+                        <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="d-flex flex-column align-items-center">
+                            <div class="bg-image hover-overlay ripple shadow-1-strong rounded" data-mdb-ripple-color="light"
+                                style="max-width: 22rem;" v-on:click="handlePFPClick">
+                                <img :src="this.userValue.photoURL" alt="" style="width: 150px; height: 150px"
+                                    class="rounded-circle mb-2" />
+
+                                <div class="mask text-light d-flex justify-content-center flex-column text-center rounded-circle"
+                                    style="background-color: rgba(0, 0, 0, 0.5); width: 150px; height: 150px">
+                                    <h4><i class="fas fa-camera"></i></h4>
+                                    <p class="m-0">Upload New</p>
+                                </div>
+                                <input type="file" ref="fileInput" accept="image/*" style="display: none"
+                                    @change="onProfilePhotoSelected">
+
+                            </div>
+                            <InputText notchWidth="10ch" id="updateDisplayName" label="Display Name"
+                                v-model="this.userValue.displayName" :isRequired="true" class="mb-3" />
+                            <InputPhoneNumber notchWidth="8ch" id="updatePhoneNumber" label="Phone Number"
+                                v-model="this.userValue.phoneNumber" :isRequired="false" class="mb-3" />
+                            <InputText notchWidth="7ch" id="updateUserPronouns" label="Pronouns"
+                                v-model="this.userValue.pronouns" :isRequired="false" />
+
+                            <p class="text-muted mb-0">{{ this.userValue.email }}</p>
+                            <p class="text-muted mb-0">{{ this.userValue.role }}</p>
+
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" @click="undoChanges">Close</button>
+                        <button type="button" class="btn btn-primary" @click="saveChanges">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  </div>
+    <!--  -->
 </template>
