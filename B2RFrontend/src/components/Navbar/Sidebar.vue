990b4bd277eb09b82825319f6762bd0ee3b9b98b
bententhhousand <bententhhousand@gmail.com>
2023-04-16 16:50:11 -0400

    Updated + Polished Sidebar and Org Chart

--- B2RFrontend/src/components/Navbar/Sidebar.vue
+++ B2RFrontend/src/components/Navbar/Sidebar.vue
@@ -1,26 +1,29 @@
 <script></script>
 
 <template>
-    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
-        <div class="position-sticky">
-            <div class="list-group list-group-flush mx-3 mt-4">
-                <a
-                    v-for="i in props.settings"
-                    href="/orgchart/#{{i}}"
-                    class="list-group-item list-group-item-action py-2 ripple"
-                    aria-current="true"
-                >
-                    <i class="fas fa-tachometer-alt fa-fw me-3"></i
-                    ><span>{{i}}</span>
-                </a>
-                            </div>
-        </div>
-    </nav>
+    <div v-if="!isMobile()">
+        <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
+            <div class="position-sticky">
+                <div class="list-group list-group-flush mx-3 mt-4">
+                    <a v-for="(value, index) in props.Links" :href="'/' + props.Parent + '/#' + value"
+                        class="list-group-item list-group-item-action py-2 ripple" aria-current="true" :key="index">
+                        <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>{{ value }}</span>
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </div>
 </template>
 <script>
  export default{
      props: {
-    settings: Object
+    Links: Object,
+    Parent: String
+  },
+  methods: {
+    isMobile() {
+            return $(window).width() < 760
+        },
   },
   setup(props){
       return{
--- B2RFrontend/src/views/OrgchartView.vue
+++ B2RFrontend/src/views/OrgchartView.vue
@@ -1,74 +1,112 @@
 <template>
     <main>
         <div class="d-flex flex-row">
-            <Sidebar :settings="roles" />
-            <div class="d-flex flex-column flex-fill">
-                <div class="d-flex flex-column mt-5" v-for="(value,key,index) in catigories">
-                    <h1 class="text-center" id="{{key}}">{{key+"s"}}</h1>
-                    <div class="row mt-5" v-for="i in Math.ceil(value.length/5)">
-                        <div v-if="(i-1)*5 < value.length" class="col">
-                            <OrgchartUser :user="value[(i-1)*5]"/>
-                        </div>
-                        <div v-if="(i-1)*5 +1 < value.length" class="col">
-                            <OrgchartUser :user="value[(i-1)*5 + 1]"/>
-                        </div>
-                        <div v-if="(i-1)*5 +2 < value.length" class="col">
-                            <OrgchartUser :user="value[(i-1)*5 + 2]"/>
-                        </div>
-                        <div v-if="(i-1)*5 +3 < value.length" class="col">
-                            <OrgchartUser :user="value[(i-1)*5 + 3]"/>
-                        </div>
-                        <div v-if="(i-1)*5 +4 < value.length" class="col">
-                            <OrgchartUser :user="value[(i-1)*5 + 4]"/>
+            <Sidebar :Links="this.roles" Parent="Orgchart" />
+            <div class="d-flex flex-column flex-fill container-fluid">
+
+                <div class="d-flex flex-column container-fluid" :id="role" v-for="(role, index) in this.roles" :key="index">
+                    <h1 class="text-center" :id="role + index">{{ pluralize(role) }}</h1>
+                    <div v-if="this.categories[role] != undefined && this.categories[role].length > 0 && !isMobile()"
+                        class="w-100">
+
+                        <div class="d-flex flex-row justify-content-center mt-5 bg-primary"
+                            v-for="i in Math.ceil(this.categories[role].length / 3)" :key="i">
+
+                            <div v-if="(i - 1) * 3 < this.categories[role].length" class="col w-33">
+                                <OrgchartUser :user="this.categories[role][(i - 1) * 3]" />
+                            </div>
+                            <div v-if="(i - 1) * 3 + 1 < this.categories[role].length" class="col w-33">
+                                <OrgchartUser :user="this.categories[role][(i - 1) * 3 + 1]" />
+                            </div>
+                            <div v-if="(i - 1) * 3 + 2 < this.categories[role].length" class="col w-33">
+                                <OrgchartUser :user="this.categories[role][(i - 1) * 3 + 2]" />
+                            </div>
+
                         </div>
                     </div>
+                    <div v-if="this.categories[role] != undefined && this.categories[role].length > 0 && isMobile()"
+                        class="w-100">
+
+                        <div class="d-flex flex-row justify-content-center mt-5 bg-primary"
+                            v-for="(value, index) in this.categories[role]" :key="index">
+                            <OrgchartUser :user="value" />
+
+                        </div>
+                    </div>
+
                 </div>
+
             </div>
         </div>
+
     </main>
 </template>
 
+
 <script>
- import OrgchartUser from "../components/Orgchart/OrgchartUser.vue";
- import Sidebar from "../components/Navbar/Sidebar.vue";
- import { db } from "../firebase";
-import { collection, doc, getDocs,setDoc, query, where } from "firebase/firestore";
+import OrgchartUser from "../components/Orgchart/OrgchartUser.vue";
+import Sidebar from "../components/Navbar/Sidebar.vue";
+import { db } from "../firebase";
+import { collection, doc, getDocs, setDoc, query, where, orderBy } from "firebase/firestore";
 import { ref, computed, watchEffect } from 'vue';
- export default{
-     setup(){
-         const catigories=ref({});
-         const roles=["Component Head","Team Mentor","Team Captain", "Manufacture"]
-         const fetchUsers = async () => {
-         const userList= collection(db,"users");
-             for(let role of roles){
-                 console.log(role)
-                 var temparr=[];
-                 const q = query(userList, where("role", "==",role ));
-                 const querySnapshot = await getDocs(q);
-                 console.log(querySnapshot.docs)
-                 for(let user of querySnapshot.docs){
-                     temparr.push(user.data());
-                 }
-                 catigories.value[role]=temparr;
-             }
-             // const querySnapshot = await getDocs(catigoryList);
-             // console.log(catigories.value.Capt)
+export default {
+    async mounted() {
+        //const categories = ref({});
+        //const roles = ["Component Head", "Team Mentor", "Team Captain", "Manufacture"]
 
-     }
-          watchEffect(() => {
-      fetchUsers();
-    });
-        return{
-            roles,
-            catigories
+
+        console.log(this.roles);
+        await this.fetchRoles();
+        await this.fetchUsers();
+
+        // watchEffect(() => {
+        //     fetchUsers();
+        // });
+    },
+    methods: {
+        isMobile() {
+            return $(window).width() < 760
+        },
+        async fetchUsers() {
+            const userList = collection(db, "users");
+            for (let role of this.roles) {
+                this.categories[role] = {};
+                console.log("Fetching " + role)
+                let temparr = [];
+                const q = query(userList, where("role", "==", role));
+                const querySnapshot = await getDocs(q);
+                console.log(querySnapshot.docs)
+                querySnapshot.forEach((doc) => {
+                    temparr.push(doc.data());
+                })
+                this.categories[role] = temparr;
+            }
+            // const querySnapshot = await getDocs(catigoryList);
+            // console.log(categories.value.Capt)
+            console.log(JSON.parse(JSON.stringify(this.categories)))
+        },
+        async fetchRoles() {
+            const roleList = collection(db, "roles");
+            const roleQ = query(roleList, orderBy('Weight', 'asc'));
+            const querySnapshotRole = await getDocs(roleQ);
+            querySnapshotRole.forEach((doc) => {
+                this.roles = [...this.roles, doc.data().Name];
+            })
+        },
+        pluralize
+    },
+    data() {
+        return {
+            roles: [],
+            categories: {}
         }
-     },
-     components: {
-OrgchartUser,
-Sidebar
+    },
+    components: {
+        OrgchartUser,
+        Sidebar
 
-  }
+    }
 
 
- }
+}
 </script>
